9be35696211b6369fb8892983da86400228b4e08
Murithi Paul <murithipaul1@gmail.com>
2025-09-29 19:59:24 +0300

    update api endpoint

--- src/api.js
+++ src/api.js
@@ -1,3 +1,3 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000/api' });
+const api = axios.create({ baseURL: process.env.REACT_APP_API_URL || 'https://eldplanner-backend-production.up.railway.app/api' });
 export default api;
